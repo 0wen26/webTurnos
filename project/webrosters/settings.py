4be351c1aaedb3faf54709e4f8d1be0e1827cfb8
owen26 <raydroid26@gmail.com>
2024-09-12 10:22:35 +0200

    ya

--- project/webrosters/settings.py
+++ project/webrosters/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.webrosters.urls'
+ROOT_URLCONF = 'webrosters.urls'
 
 TEMPLATES = [
     {
@@ -57,7 +57,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.webrosters.wsgi.application'
+WSGI_APPLICATION = 'webrosters.wsgi.application'
 
 
 # Database
