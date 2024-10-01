b7c4906ec98aaea160044ae870b65971f5d62926
owen26 <raydroid26@gmail.com>
2024-10-02 00:36:07 +0200

    ye

--- start.sh
+++ start.sh
@@ -8,12 +8,12 @@ python manage.py migrate
 
 #echo "from turnos.models import Turno; Turno.objects.all().delete()" | python manage.py shell
 
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin', is_superuser=True).delete()" | python manage.py shell
-echo "Superusuario eliminado"
+#echo "from django.contrib.auth.models import User; User.objects.filter(username='admin', is_superuser=True).delete()" | python manage.py shell
+#echo "Superusuario eliminado"
 
 # Crear un superusuario si no existe
 if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Burgos09')" | python manage.py shell
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('owencin', 'admin@example.com', 'Burgos09')" | python manage.py shell
     echo "Superusuario creado"
 else
     echo "El superusuario ya existe, no se ha creado ninguno nuevo."
