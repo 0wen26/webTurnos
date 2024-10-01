66b170fc462032ba27d4390e8652e420a8438dcc
owen26 <raydroid26@gmail.com>
2024-10-02 00:39:44 +0200

    yea

--- start.sh
+++ start.sh
@@ -12,12 +12,8 @@ python manage.py migrate
 #echo "Superusuario eliminado"
 
 # Crear un superusuario si no existe
-if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('owencin', 'admin@example.com', 'Burgos09')" | python manage.py shell
-    echo "Superusuario creado"
-else
-    echo "El superusuario ya existe, no se ha creado ninguno nuevo."
-fi
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('owencin', 'admin@example.com', 'Burgos09')" | python manage.py shell
+echo "Superusuario creado"
 
 #gunicorn webrosters.wsgi:application  Iniciar Gunicorn (o el servidor que estés utilizando)
 gunicorn webrosters.wsgi:application #--bind 0.0.0.0:8000
