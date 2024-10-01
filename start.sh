64e35f09e5350913e4ffadf4646fb0cd84733311
owen26 <raydroid26@gmail.com>
2024-10-01 16:25:15 +0200

    super

--- start.sh
+++ start.sh
@@ -5,14 +5,17 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Crear un superusuario si no existe
-if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Burgos09')" | python manage.py shell
-    echo "Superusuario creado"
-else
-    echo "El superusuario ya existe, no se ha creado ninguno nuevo."
-fi
+#if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
+#    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Burgos09')" | python manage.py shell
+#    echo "Superusuario creado"
+#else
+#    echo "El superusuario ya existe, no se ha creado ninguno nuevo."
+#fi
 
-echo "from turnos.models import Turno; Turno.objects.all().delete()" | python manage.py shell
+#echo "from turnos.models import Turno; Turno.objects.all().delete()" | python manage.py shell
+
+echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.set_password('Burgos09'); user.save()" | python manage.py shell
+
 
 #gunicorn webrosters.wsgi:application  Iniciar Gunicorn (o el servidor que estés utilizando)
 gunicorn webrosters.wsgi:application #--bind 0.0.0.0:8000
