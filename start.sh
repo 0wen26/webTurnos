f935544a077ac06cdedc88e030c0b0377dc10b49
owen26 <raydroid26@gmail.com>
2024-10-01 16:41:31 +0200

    super

--- start.sh
+++ start.sh
@@ -4,18 +4,20 @@
 python manage.py makemigrations
 python manage.py migrate
 
-# Crear un superusuario si no existe
-#if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
-#    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Burgos09')" | python manage.py shell
-#    echo "Superusuario creado"
-#else
-#    echo "El superusuario ya existe, no se ha creado ninguno nuevo."
-#fi
+
 
 #echo "from turnos.models import Turno; Turno.objects.all().delete()" | python manage.py shell
 
-echo "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.set_password('Burgos09'); user.save()" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin', is_superuser=True).delete()" | python manage.py shell
+echo "Superusuario eliminado"
 
+# Crear un superusuario si no existe
+if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell | tail -n 1)" ]; then
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Burgos09')" | python manage.py shell
+    echo "Superusuario creado"
+else
+    echo "El superusuario ya existe, no se ha creado ninguno nuevo."
+fi
 
 #gunicorn webrosters.wsgi:application  Iniciar Gunicorn (o el servidor que estés utilizando)
 gunicorn webrosters.wsgi:application #--bind 0.0.0.0:8000
--- webrosters/settings.py
+++ webrosters/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webrosters.urls'
@@ -138,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
